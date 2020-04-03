bbcf2a877195b08ce9dd8b4a320f161cc26c4198
Jarryd Stamatelos <jarryd.stamatelos.com>
2020-04-03 16:51:08 -0400

    test post

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const { sendRegistrationEmail } = require('./routes/sendMail')
-const parser = require('body-parser')
 const port = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
-app.use(parser)
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World' }))
