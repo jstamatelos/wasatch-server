b978676a55ca3a6a8aaec4507a6e38d310191e56
Jarryd Stamatelos <jarryd.stamatelos.com>
2020-04-03 16:19:17 -0400

    update variables, allow send to multiple targets

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,6 +1,11 @@
 const sendgrid = require('@sendgrid/mail')
 sendgrid.setApiKey(process.env.SEND_GRID_API_KEY)
 const sender = process.env.SENDER
+const subject = process.env.SUBJECT
+const staff = process.env.WASATCH_STAFF
+const staff0 = staff.split('|')[0]
+const staff1 = staff.split('|')[1]
+const staff2 = staff.split('|')[2]
 
 const joi = require('@hapi/joi')
 const boom = require('@hapi/boom')
@@ -17,28 +22,27 @@ const sendRegistrationEmail = async (req, res) => {
         await schema.validateAsync(req.body)
     } catch (error) {
         const { output } = boom.badData()
+        console.error(output)
         res.send(output)
     }
     const msg = {
-        to: 'swdeveloper@jarrydstamatelos.com',
+        to: [{
+            email: staff0
+        },{
+            email: staff1
+        },{
+            email: staff2
+        }],
         from: sender,
-        subject: 'New Registration Request',
+        subject: subject,
         html: `Name: ${firstName} ${lastName}, ${registrantEmail} wants to register for Wasatch LC`
     }
+    
     sendgrid.send(msg)
 }
 
-const req = {
-    body: {
-        firstName: 'Alistair',
-        lastName: 'Beckett',
-        registrantEmail: 'aMadeUpEmailAddress@test.com'
-    }
+module.exports = {
+    sendRegistrationEmail
 }
-
-sendRegistrationEmail(req)
-// module.exports = {
-//     sendRegistrationEmail
-// }
 
 
