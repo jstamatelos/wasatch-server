13d569e90e484c16b4188776b1bd69a68514ea7f
Jarryd Stamatelos <jarryd.stamatelos.com>
2020-10-30 13:00:51 -0400

    log

--- app.js
+++ app.js
@@ -11,18 +11,19 @@ const app = express()
 app.use(express.json())
 app.use(pino)
 app.use(cors())
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://jstamatelos.github.io/') // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-app.get('/', function (req, res) {
+const corsOptions = {
+  origin: 'https://jstamatelos.github.io/'
+}
+app.get('/', cors(corsOptions), function (req, res) {
+  req.log.info('get () :: attempting to open connection to server...')
   res.send(JSON.stringify({ Status: 'Running' }))
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', cors(corsOptions), async (req, res) => {
   req.log.info('register() :: attempting to create registration...')
   try {
+    req.log.info('register() :: attempt to validate request')
+
     await isValid(req)
     // await sendRegistrationEmail(req)
     saveUser(req.body)
